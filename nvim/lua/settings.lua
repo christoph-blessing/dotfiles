72e0c6aea20302b19dc9f0c6fe58aa05650979c1
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-03-30 21:37:38 +0200

    Add some general settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,11 +1,20 @@
 vim.o.clipboard = "unnamedplus"
 vim.o.updatetime = 100
+vim.o.timeoutlen = 100
 vim.o.termguicolors = true
 vim.o.hidden = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.showmode = false
 
 vim.wo.spell = true
 vim.wo.number = true
 vim.wo.relativenumber = true
+vim.wo.wrap = false
+vim.wo.cursorline = true
+vim.wo.signcolumn = "yes"
 
 vim.bo.expandtab = true
 vim.bo.spelllang = "en_us"
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
