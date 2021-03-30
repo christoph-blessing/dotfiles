b1d3a098a925479dcdfed1918e53a4292dd4a017
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-03-30 21:37:08 +0200

    Show vi mode with color in statusline

--- nvim/lua/nv-galaxyline.lua
+++ nvim/lua/nv-galaxyline.lua
@@ -27,41 +27,74 @@ require"galaxyline".short_line_list = {"NvimTree", "packer"}
 -- Left --
 
 require"galaxyline".section.left[1] = {
+    ViMode = {
+        provider = function()
+            local mode_color = {
+                n = colors.blue,
+                i = colors.orange,
+                v = colors.purple,
+                V = colors.purple,
+                [""] = colors.purple,
+                c = colors.red,
+                no = colors.blue,
+                s = colors.green,
+                S = colors.green,
+                [""] = colors.green,
+                ic = colors.yellow,
+                R = colors.aqua,
+                Rv = colors.aqua,
+                cv = colors.blue,
+                ce = colors.blue,
+                r = colors.light_blue,
+                rm = colors.light_blue,
+                ["!"] = colors.blue,
+                t = colors.blue
+            }
+            vim.api.nvim_command("hi GalaxyViMode guifg=" .. mode_color[vim.fn.mode()])
+            return "▊"
+        end,
+        highlight = {"NONE", colors.bg1},
+        separator = "",
+        separator_highlight = {colors.bg1, colors.fg1}
+    }
+}
+
+require"galaxyline".section.left[2] = {
     GitBranch = {
         provider = "GitBranch",
         condition = condition.check_git_workspace,
-        icon = "    ",
+        icon = "   ",
         highlight = {colors.bg1, colors.fg1},
         separator = "",
         separator_highlight = {colors.fg1, colors.bg1}
     }
 }
 
-require"galaxyline".section.left[2] = {
+require"galaxyline".section.left[3] = {
     DiffAdd = {provider = "DiffAdd", icon = "  ", highlight = {colors.green, colors.bg1}}
 }
 
-require"galaxyline".section.left[3] = {
+require"galaxyline".section.left[4] = {
     DiffModified = {provider = "DiffModified", icon = "  ", highlight = {colors.blue, colors.bg1}}
 }
 
-require"galaxyline".section.left[4] = {
+require"galaxyline".section.left[5] = {
     DiffRemove = {provider = "DiffRemove", icon = "  ", highlight = {colors.red, colors.bg1}}
 }
 
-require"galaxyline".section.left[5] = {
+require"galaxyline".section.left[6] = {
     DiagnosticError = {provider = "DiagnosticError", icon = "  ", highlight = {colors.red, colors.bg1}}
 }
 
-require"galaxyline".section.left[6] = {
+require"galaxyline".section.left[7] = {
     DiagnosticWarning = {provider = "DiagnosticWarn", icon = "  ", highlight = {colors.orange, colors.bg1}}
 }
 
-require"galaxyline".section.left[7] = {
+require"galaxyline".section.left[8] = {
     DiagnosticHint = {provider = "DiagnosticHint", icon = "  ", highlight = {colors.blue, colors.bg1}}
 }
 
-require"galaxyline".section.left[8] = {
+require"galaxyline".section.left[9] = {
     DiagnosticInfo = {provider = "DiagnosticInfo", icon = "  ", highlight = {colors.aqua, colors.bg1}}
 }
 
