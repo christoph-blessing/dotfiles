4057b378e5a77c385f9697fcad22f8494fa69de2
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-03-30 21:38:43 +0200

    Configure nvimtree plugin

--- nvim/lua/nv-tree.lua
+++ nvim/lua/nv-tree.lua
@@ -1,1 +1,6 @@
+vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_hide_dotfiles = 1
+vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_indent_markers = 1
+
 vim.api.nvim_set_keymap("n", "<C-n>", "<CMD>NvimTreeToggle<CR>", {noremap = true, silent = true})
