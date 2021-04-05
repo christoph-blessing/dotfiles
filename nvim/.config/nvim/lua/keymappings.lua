d8cfe02bb89f7827f56ac2eea3aac8375f84f90a
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-04-05 19:34:50 +0200

    Add goyo and undotree plugins

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -8,3 +8,5 @@ vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", {silent = true})
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", {silent = true})
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", {silent = true})
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", {silent = true})
+
+vim.api.nvim_set_keymap("n", "<F5>", ":UndotreeToggle<CR>", {noremap = true, silent = true})
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -25,4 +25,6 @@ return require"packer".startup(function()
     use {"nvim-telescope/telescope.nvim", requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}}
     use "mfussenegger/nvim-dap"
     use {"lukas-reineke/indent-blankline.nvim", branch = "lua"}
+    use "junegunn/goyo.vim"
+    use "mbbill/undotree"
 end)
