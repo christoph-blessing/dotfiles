14b80a209e703040840db64e2671d8d936b65f01
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-05-01 00:01:16 +0200

    Add command for running python script

--- nvim/.config/nvim/lua/nv-autocommands.lua
+++ nvim/.config/nvim/lua/nv-autocommands.lua
@@ -5,6 +5,15 @@ nvim_create_augroups({
         {"BufWritePre", "*.py", "lua vim.lsp.buf.formatting_sync(nil, 1000)"},
         {"BufWritePre", "*.lua", "lua vim.lsp.buf.formatting_sync(nil, 1000)"}
 
+    },
+    run = {
+        {
+            "FileType", "python",
+            "lua vim.api.nvim_set_keymap('n', '<f9>', \":w<cr>:exec '!python' shellescape(@%, 1)<cr>\", {})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_set_keymap('i', '<f9>', \"<esc>:w<cr>:exec '!python' shellescape(@%, 1)<cr>\", {})"
+        }
     }
 })
 
