8aee856fb149ce72dd30b991218fe6d699976b42
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-05-04 11:27:14 +0200

    Add keybindings for plantuml

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -6,13 +6,22 @@ nvim_create_augroups({
         {"BufWritePre", "*.lua", "lua vim.lsp.buf.formatting_sync(nil, 1000)"}
 
     },
-    run = {
+    python = {
         {
             "FileType", "python",
             "lua vim.api.nvim_set_keymap('n', '<leader>r', \":w<cr>:exec '!python' shellescape(@%, 1)<cr>\", {})"
         }, {
             "FileType", "python",
             "lua vim.api.nvim_set_keymap('i', '<leader>r', \"<esc>:w<cr>:exec '!python' shellescape(@%, 1)<cr>\", {})"
+        }
+    },
+    plantuml = {
+        {
+            "FileType", "plantuml",
+            "lua vim.api.nvim_set_keymap('n', '<leader>r', \":w !plantuml -p | ~/.config/nvim/scripts/sxiv.sh<cr>\", {})"
+        }, {
+            "FileType", "plantuml",
+            "lua vim.api.nvim_set_keymap('i', '<leader>r', \"<esc>:w !plantuml -p | ~/.config/nvim/scripts/sxiv.sh<cr>\", {})"
         }
     }
 })
