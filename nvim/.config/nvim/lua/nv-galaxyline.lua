0eec7bcd1f266f81346711e524337d867815755c
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-05-19 22:18:59 +0200

    Remove color indicator from statusline

--- nvim/.config/nvim/lua/nv-galaxyline.lua
+++ nvim/.config/nvim/lua/nv-galaxyline.lua
@@ -1,26 +1,5 @@
 local condition = require "galaxyline.condition"
 
--- local colors = {
---     bg = "#282828",
---     bg1 = "#3c3836",
---     fg = "#ebdbb2",
---     fg1 = "#d5c4a1",
---     red = "#cc241d",
---     green = "#98971a",
---     yellow = "#d79921",
---     blue = "#458588",
---     purple = "#b16286",
---     aqua = "#689d6a",
---     gray = "#a89984",
---     orange = "#d65d0e",
---     light_red = "#fb4934",
---     light_green = "#b8bb26",
---     light_yellow = "#fabd2f",
---     light_blue = "#83a598",
---     light_purple = "#d3869b",
---     light_aqua = "#8ec07c",
---     light_orange = "#fe8019"
--- }
 local colors = {
     bg0 = "#101010",
     bg1 = "#262727",
@@ -62,66 +41,33 @@ require"galaxyline".short_line_list = {"NvimTree", "packer"}
 -- Left --
 
 require"galaxyline".section.left[1] = {
-    ViMode = {
-        provider = function()
-            local mode_color = {
-                n = colors.blue,
-                i = colors.orange,
-                v = colors.purple,
-                V = colors.purple,
-                [""] = colors.purple,
-                c = colors.red,
-                no = colors.blue,
-                s = colors.green,
-                S = colors.green,
-                [""] = colors.green,
-                ic = colors.yellow,
-                R = colors.aqua,
-                Rv = colors.aqua,
-                cv = colors.blue,
-                ce = colors.blue,
-                r = colors.light_blue,
-                rm = colors.light_blue,
-                ["!"] = colors.blue,
-                t = colors.blue
-            }
-            vim.api.nvim_command("hi GalaxyViMode guifg=" .. mode_color[vim.fn.mode()])
-            return "▊"
-        end,
-        highlight = {colors.none, colors.bg_statusline2},
-        separator = "",
-        separator_highlight = {colors.bg_statusline2, colors.fg0}
-    }
-}
-
-require"galaxyline".section.left[2] = {
     GitBranch = {
         provider = "GitBranch",
         condition = condition.check_git_workspace,
-        icon = "   ",
+        icon = "    ",
         highlight = {colors.bg_statusline2, colors.fg0},
         separator = "",
         separator_highlight = {colors.fg0, colors.bg_statusline2}
     }
 }
 
-require"galaxyline".section.left[3] = {
+require"galaxyline".section.left[2] = {
     DiffAdd = {provider = "DiffAdd", icon = "  ", highlight = {colors.green, colors.bg_statusline2}}
 }
 
-require"galaxyline".section.left[4] = {
+require"galaxyline".section.left[3] = {
     DiffModified = {provider = "DiffModified", icon = "  ", highlight = {colors.blue, colors.bg_statusline2}}
 }
 
-require"galaxyline".section.left[5] = {
+require"galaxyline".section.left[4] = {
     DiffRemove = {provider = "DiffRemove", icon = "  ", highlight = {colors.red, colors.bg_statusline2}}
 }
 
-require"galaxyline".section.left[6] = {
+require"galaxyline".section.left[5] = {
     DiagnosticError = {provider = "DiagnosticError", icon = "  ", highlight = {colors.red, colors.bg_statusline2}}
 }
 
-require"galaxyline".section.left[7] = {
+require"galaxyline".section.left[6] = {
     DiagnosticWarning = {
         provider = "DiagnosticWarn",
         icon = "  ",
@@ -129,11 +75,11 @@ require"galaxyline".section.left[7] = {
     }
 }
 
-require"galaxyline".section.left[8] = {
+require"galaxyline".section.left[7] = {
     DiagnosticHint = {provider = "DiagnosticHint", icon = "  ", highlight = {colors.blue, colors.bg_statusline2}}
 }
 
-require"galaxyline".section.left[9] = {
+require"galaxyline".section.left[8] = {
     DiagnosticInfo = {provider = "DiagnosticInfo", icon = "  ", highlight = {colors.aqua, colors.bg_statusline2}}
 }
 
