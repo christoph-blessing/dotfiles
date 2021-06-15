76e5e2131036bedfcbe6a4996d3f66a19dcd6e0b
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-06-15 12:04:57 +0200

    Add debugging keybindings

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -13,8 +13,34 @@ nvim_create_augroups({
         }, {
             "FileType", "python",
             "lua vim.api.nvim_buf_set_keymap('0', 'n', '<leader>tu', \":w<cr>:exec '!pytest tests/unit'<cr>\", {})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<F5>', \":lua require'dap'.continue()<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<F10>', \":lua require'dap'.step_over()<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<F11>', \":lua require'dap'.step_into()<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<F12>', \":lua require'dap'.step_out()<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<leader>b', \":lua require'dap'.toggle_breakpoint()<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<leader>B', \":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<leader>lp', \":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<leader>dr', \":lua require'dap'.repl.open()<CR>\", {silent = true})"
+        }, {
+            "FileType", "python",
+            "lua vim.api.nvim_buf_set_keymap('0', 'n', '<leader>dl', \":lua require'dap'.run_last()<CR>\", {silent = true})"
         }
-
     },
     plantuml = {
         {
