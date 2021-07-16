4250119b353aa0e5af08d0297f013903b89eeec7
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-07-16 12:00:45 +0200

    Fix backslashes disappearing

--- nvim/.config/nvim/scripts/mypy.sh
+++ nvim/.config/nvim/scripts/mypy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tempfile=$(mktemp /tmp/shadow_file.XXXXXX)
-printf "$(cat)" >> "$tempfile"
+printf "%s\n" "$(cat)" >> "$tempfile"
 pdm run mypy --show-column-numbers --shadow-file "$1" "$tempfile" "$1"
 rm "$tempfile"
