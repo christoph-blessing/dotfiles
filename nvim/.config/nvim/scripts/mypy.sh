2a8a71cefb2e88165359b66d4d9c346af127d89b
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-07-26 10:41:30 +0200

    Ignore test files

--- nvim/.config/nvim/scripts/mypy.sh
+++ nvim/.config/nvim/scripts/mypy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $1 == *test_* ]]
+then
+    exit
+fi
+
 tempfile=$(mktemp /tmp/shadow_file.XXXXXX)
 printf "%s\n" "$(cat)" >> "$tempfile"
 pdm run mypy --show-column-numbers --shadow-file "$1" "$tempfile" "$1"
