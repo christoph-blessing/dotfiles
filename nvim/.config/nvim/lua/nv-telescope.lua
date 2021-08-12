d53d146c668ad8e0f24c4fb864da116200bb6bc8
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-08-12 21:21:40 +0200

    Add keybindings for lsp sources in telescope

--- nvim/.config/nvim/lua/nv-telescope.lua
+++ nvim/.config/nvim/lua/nv-telescope.lua
@@ -4,3 +4,8 @@ vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin'
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+
+vim.api.nvim_set_keymap("n", "<leader>fr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>fd", "<cmd>lua require('telescope.builtin').lsp_document_diagnostics()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>fwd", "<cmd>lua require('telescope.builtin').lsp_workspace_diagnostics()<cr>",
+                        opts)
