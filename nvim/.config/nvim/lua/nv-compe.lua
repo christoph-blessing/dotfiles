a493e219570d885c0fa4ec8c1358798ac41260fd
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-08-21 00:19:05 +0200

    Update compe config

--- nvim/.config/nvim/lua/nv-compe.lua
+++ nvim/.config/nvim/lua/nv-compe.lua
@@ -1,56 +1,64 @@
 vim.o.completeopt = "menuone,noselect"
 
-require"compe".setup {
+require'compe'.setup {
     enabled = true,
     autocomplete = true,
     debug = false,
     min_length = 1,
-    preselect = "enable",
+    preselect = 'enable',
     throttle_time = 80,
     source_timeout = 200,
+    resolve_timeout = 800,
     incomplete_delay = 400,
     max_abbr_width = 100,
     max_kind_width = 100,
     max_menu_width = 100,
-    documentation = true,
+    documentation = {
+        border = {'', '', '', ' ', '', '', '', ' '},
+        winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+        max_width = 120,
+        min_width = 60,
+        max_height = math.floor(vim.o.lines * 0.3),
+        min_height = 1
+    },
 
     source = {path = true, buffer = true, calc = true, nvim_lsp = true, nvim_lua = true, vsnip = true}
 }
-
-local replace_termcodes = function(str) return vim.api.nvim_replace_termcodes(str, true, true, true) end
-
-local check_back_space = function()
-    local col = vim.fn.col(".") - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
-end
-
-_G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return replace_termcodes "<C-n>"
-    elseif vim.fn.call("vsnip#available", {1}) == 1 then
-        return replace_termcodes "<Plug>(vsnip-expand-or-jump)"
-    elseif check_back_space() then
-        return replace_termcodes "<Tab>"
-    else
-        return vim.fn["compe#complete"]()
-    end
-end
-
-_G.shift_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return replace_termcodes "<C-p>"
-    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-        return replace_termcodes "<Plug>(vsnip-jump-prev)"
-    else
-        return replace_termcodes "<S-Tab>"
-    end
-end
 
 vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", {silent = true, noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<C-e>", "compe#close('<C-e>')", {silent = true, noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<C-f>", "compe#scroll({ 'delta': +4 })", {silent = true, noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<C-d>", "compe#scroll({ 'delta': -4 })", {silent = true, noremap = true, expr = true})
+
+local t = function(str) return vim.api.nvim_replace_termcodes(str, true, true, true) end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif vim.fn['vsnip#available'](1) == 1 then
+        return t "<Plug>(vsnip-expand-or-jump)"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn['compe#complete']()
+    end
+end
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+        return t "<Plug>(vsnip-jump-prev)"
+    else
+        return t "<S-Tab>"
+    end
+end
+
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.shift_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.shift_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
