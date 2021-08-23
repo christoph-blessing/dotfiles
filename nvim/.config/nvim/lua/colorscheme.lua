0423b9231dd375b9f4edab5238367277ba4a9fab
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-08-23 21:10:41 +0200

    Use regular gruvbox colorscheme

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,4 +1,3 @@
-vim.g.gruvbox_material_palette = "material"
-vim.g.gruvbox_material_background = "medium"
-vim.g.gruvbox_material_transparent_background = 0
-vim.cmd("colorscheme gruvbox-material")
+vim.g.gruvbox_contrast_dark = "soft"
+
+vim.cmd("colorscheme gruvbox")
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,7 +2,7 @@ vim.cmd("autocmd BufWritePost plugins.lua PackerCompile")
 
 return require("packer").startup(function()
 	use("wbthomason/packer.nvim")
-	use("sainnhe/gruvbox-material")
+	use("morhetz/gruvbox")
 	use("tpope/vim-surround")
 	use("neovim/nvim-lspconfig")
 	use("hrsh7th/nvim-compe")
