b27f242cd6a0a1915b870f666b80ee1c824613e7
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-08-29 21:35:26 +0200

    Shift tabline when opening/closing file tree

--- nvim/.config/nvim/lua/nv-tree.lua
+++ nvim/.config/nvim/lua/nv-tree.lua
@@ -1,12 +1,39 @@
-M = {}
+local M = {}
 
 M.setup = function()
 	vim.g.nvim_tree_auto_close = 1
 	vim.g.nvim_tree_hide_dotfiles = 1
 	vim.g.nvim_tree_quit_on_open = 1
 	vim.g.nvim_tree_indent_markers = 1
+	vim.g.nvim_tree_width = 30
 
 	vim.api.nvim_set_keymap("n", "<C-n>", "<CMD>NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+	local tree_view = require("nvim-tree.view")
+
+	local open = tree_view.open
+	tree_view.open = function()
+		M.on_open()
+		open()
+	end
+
+	local close = tree_view.close
+	tree_view.close = function()
+		M.on_close()
+		close()
+	end
+end
+
+M.on_open = function()
+	if package.loaded["bufferline.state"] then
+		require("bufferline.state").set_offset(vim.g.nvim_tree_width + 1, "FileTree")
+	end
+end
+
+M.on_close = function()
+	if package.loaded["bufferline.state"] then
+		require("bufferline.state").set_offset(0)
+	end
 end
 
 return M
