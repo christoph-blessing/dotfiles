ad6c50da2d7ba4f530e04cd44d083c7d7dfc36b6
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-09-16 20:19:46 +0200

    Use node ids to select test

--- nvim/.config/nvim/lua/pytest.lua
+++ nvim/.config/nvim/lua/pytest.lua
@@ -1,5 +1,15 @@
 local function starts_with(str, start)
-	return str:sub(1, #start) == start
+	return str:lower():sub(1, #start) == start
+end
+
+local function reverse(list)
+	local n = #list
+	local i = 1
+	while i < n do
+		list[i], list[n] = list[n], list[i]
+		i = i + 1
+		n = n - 1
+	end
 end
 
 local M = {}
@@ -12,7 +22,7 @@ M.run_test_under_cursor = function()
 	while node ~= nil do
 		local node_type = node:type()
 		if node_type == "function_definition" or node_type == "class_definition" then
-			local ident = ts_utils.get_node_text(node:named_child(0))[1]:lower()
+			local ident = ts_utils.get_node_text(node:named_child(0))[1]
 			if not starts_with(ident, "test") then
 				break
 			end
@@ -21,10 +31,11 @@ M.run_test_under_cursor = function()
 		node = node:parent()
 	end
 
-	local pytest_expr = table.concat(parts, " and ")
+	reverse(parts)
+	local path = table.concat(parts, "::")
 
-	if pytest_expr ~= "" then
-		require("toggleterm").exec("pytest -k '" .. pytest_expr .. "' " .. vim.fn.expand("%"))
+	if path ~= "" then
+		require("toggleterm").exec("pytest " .. vim.fn.expand("%") .. "::" .. path)
 	end
 end
 
