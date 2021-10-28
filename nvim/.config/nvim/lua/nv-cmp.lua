f922a0b69712db80c2ab153e20c7cd9a6698b2e4
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-10-28 16:51:38 +0200

    Fix broken TAB and CR mappings

--- nvim/.config/nvim/lua/nv-cmp.lua
+++ nvim/.config/nvim/lua/nv-cmp.lua
@@ -13,26 +13,21 @@ cmp.setup({
 	},
 
 	mapping = {
-		["<Tab>"] = function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
-			elseif check_back_space() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n")
-			elseif vim.fn["vsnip#available"]() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>(vsnip-expand-or-jump)", true, true, true), "")
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
 			else
 				fallback()
 			end
-		end,
-		["<S-Tab>"] = function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
-			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>(vsnip-jump-prev)", true, true, true), "")
+		end),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
 			else
 				fallback()
 			end
-		end,
+		end),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
