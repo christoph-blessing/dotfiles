a511204eb217195dabda1245066f9485c237d183
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-11-11 17:29:05 +0100

    Use luasnip with nvim-cmp

--- nvim/.config/nvim/lua/nv-cmp.lua
+++ nvim/.config/nvim/lua/nv-cmp.lua
@@ -1,14 +1,17 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_back_space = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
+
 	snippet = {
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
 
@@ -16,6 +19,10 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
@@ -23,6 +30,8 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
@@ -35,9 +44,9 @@ cmp.setup({
 	},
 
 	sources = {
-		{ name = "buffer" },
 		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
 		{ name = "path" },
-		{ name = "vsnip" },
 	},
 })
