bc94d46bf18013dbc8028a3c42018b440b3b0472
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-12-10 16:34:27 +0100

    Change key binding for exiting terminal mode

--- nvim/.config/nvim/lua/nv-toggleterm.lua
+++ nvim/.config/nvim/lua/nv-toggleterm.lua
@@ -20,7 +20,7 @@ end
 M.set_terminal_keymaps = function()
 	local opts = { noremap = true }
 
-	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-n>", [[<C-\><C-n>]], opts)
 
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
