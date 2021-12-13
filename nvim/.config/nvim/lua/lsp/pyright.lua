62d1fa810bc0e7a5d8e873a94321d048982dcfff
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-12-13 15:24:53 +0100

    Run pyright in Docker container

--- nvim/.config/nvim/lua/lsp/pyright.lua
+++ nvim/.config/nvim/lua/lsp/pyright.lua
@@ -1,1 +1,23 @@
-return { cmd = { vim.fn.stdpath("data") .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio" } }
+local resolve_cmd = function()
+	local pipe = io.popen("docker-compose config --services")
+	for line in pipe:lines() do
+		if line == "dev" then
+			local root_dir = vim.fn.getcwd()
+			return {
+				"docker-compose",
+				"run",
+				"--rm",
+				"dev",
+			}
+		end
+	end
+	return { vim.fn.stdpath("data") .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio" }
+end
+
+return {
+	before_init = function(params)
+		params.processId = vim.NIL
+	end,
+	cmd = resolve_cmd(),
+	root_dir = require("lspconfig").util.root_pattern(".git", vim.fn.getcwd()),
+}
