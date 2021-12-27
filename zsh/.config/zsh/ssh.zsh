b5f2c8b3edb73ae56da6ae5ec300b81024dffef1
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-12-27 15:41:48 +0100

    Move ssh agent related zsh config to new file

---
+++ zsh/.config/zsh/ssh.zsh
@@ -0,0 +1,10 @@
+# SSH agent
+if ! [ -z "${XDG_RUNTIME_DIR}" ]; then
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+        ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+    fi
+    if (( ! ${+SSH_AUTH_SOCK} )); then
+        source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+    fi
+fi
+
