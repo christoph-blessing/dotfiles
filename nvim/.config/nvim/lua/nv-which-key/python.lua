bac13ca6a10346cfb5a217b1f4159e8438e6186f
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-02-28 17:06:48 +0100

    Add updated pytest keybindings

--- nvim/.config/nvim/lua/nv-which-key/python.lua
+++ nvim/.config/nvim/lua/nv-which-key/python.lua
@@ -5,11 +5,9 @@ M.setup = function()
 	local mappings = {
 		t = {
 			name = "Testing",
-			a = { "<cmd>w<cr><cmd>TermExec cmd='pytest'<cr>", "Run All Tests" },
+			a = { "<cmd>w<cr><cmd>lua require('pytest').run_tests()<cr>", "Run All Tests" },
+			d = { "<cmd>w<cr><cmd>lua require('pytest').run_tests_in_file()<cr>", "Run All Tests In File" },
 			c = { "<cmd>w<cr><cmd>lua require('pytest').run_test_under_cursor()<cr>", "Run Test Under Cursor" },
-			d = { "<cmd>w<cr><cmd>TermExec cmd='pytest %'<cr>", "Run All Tests In File" },
-			f = { "<cmd>w<cr><cmd>TermExec cmd='pytest tests/functional'<cr>", "Run Functional Tests" },
-			u = { "<cmd>w<cr><cmd>TermExec cmd='pytest tests/unit'<cr>", "Run Unit Tests" },
 		},
 		r = { "<cmd>w<cr><cmd>TermExec cmd='python3 %'<cr>", "Run Current File" },
 	}
