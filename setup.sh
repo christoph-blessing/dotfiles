6027f53248019a0420dcec1287b933b9fcf7b784
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-03-29 13:17:39 +0200

    Remove setup script

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e 
-
-dotfiles_dir="$HOME"/.local/share/dotfiles
-
-git clone https://github.com/cblessing24/dotfiles.git "$dotfiles_dir"
-stow --dir "$dotfiles_dir" --target "$HOME" flake8 git nvim pylint tmux zsh zshenv
