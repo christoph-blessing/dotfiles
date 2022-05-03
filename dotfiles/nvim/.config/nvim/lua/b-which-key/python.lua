c6122d02246b6ed34b1c1fe404ad7fa3b12cda06
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-03 17:12:56 +0200

    Add more ultest keybindings

--- dotfiles/nvim/.config/nvim/lua/b-which-key/python.lua
+++ dotfiles/nvim/.config/nvim/lua/b-which-key/python.lua
@@ -5,10 +5,12 @@ M.setup = function()
 	local mappings = {
 		t = {
 			name = "Testing",
-			f = { "<cmd>w<cr><cmd>Ultest<cr>" ,"Run All Tests In File" },
-			n = { "<cmd>w<cr><cmd>UltestNearest<cr>", "Run Nearest Test" },
-			l = { "<cmd>w<cr><cmd>UltestLast<cr>", "Run Last Ran Test" },
 			c = { "<cmd>w<cr><cmd>UltestClear<cr>", "Clear Test Output" },
+			f = { "<cmd>w<cr><cmd>Ultest<cr>", "Run All Tests In File" },
+			l = { "<cmd>w<cr><cmd>UltestLast<cr>", "Run Last Ran Test" },
+			n = { "<cmd>w<cr><cmd>UltestNearest<cr>", "Run Nearest Test" },
+			o = { "<cmd>w<cr><cmd>UltestOutput<cr>", "Show Output" },
+			s = { "<cmd>w<cr><cmd>UltestSummary<cr>", "Toggle Summary" },
 		},
 		r = { "<cmd>w<cr><cmd>TermExec cmd='python3 %'<cr>", "Run Current File" },
 	}
