bf087e8fa64719fc67f927d6e0b5a9b19d72f741
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-03 10:59:12 +0200

    Rename null-ls config

--- dotfiles/nvim/.config/nvim/lua/lsp/b-null-ls.lua
+++ dotfiles/nvim/.config/nvim/lua/lsp/b-null-ls.lua
@@ -42,11 +42,14 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach = function(client, bufnr)
 	if client.supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd(
-			"BufWritePre",
-			{ group = augroup, buffer = bufnr, callback = vim.lsp.buf.formatting_sync }
-		)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format()
+			end,
+		})
 	end
 end
 
-null_ls.setup({ sources = sources, on_attach = on_attach })
+null_ls.setup({ sources = sources, on_attach = on_attach, debug = false })
