5ac79299e558190dd4a7869403006657b0e3d8fc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-23 17:23:07 +0200

    Disable neotest-summary filetype in lualine

--- dotfiles/nvim/.config/nvim/lua/b-lualine.lua
+++ dotfiles/nvim/.config/nvim/lua/b-lualine.lua
@@ -7,7 +7,7 @@ M.setup = function()
 			theme = "auto",
 			component_separators = { left = "", right = "" },
 			section_separators = { left = "", right = "" },
-			disabled_filetypes = { "toggleterm", "NvimTree" },
+			disabled_filetypes = { "toggleterm", "NvimTree", "neotest-summary" },
 		},
 		sections = {
 			lualine_a = { "mode" },
