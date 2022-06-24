e7d2d3b0d8efa6ac90645d68dc1c9660ae8d2d35
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-24 13:42:47 +0200

    Disable copilot for dap-repl

--- dotfiles/nvim/.config/nvim/lua/b-copilot.lua
+++ dotfiles/nvim/.config/nvim/lua/b-copilot.lua
@@ -3,6 +3,9 @@ local M = {}
 M.setup = function()
 	vim.g.copilot_no_tab_map = true
 	vim.g.copilot_assume_mapped = true
+	vim.g.copilot_filetypes = {
+		["dap-repl"] = false,
+	}
 	vim.api.nvim_set_keymap("i", "<C-J>", "copilot#Accept('<CR>')", { silent = true, expr = true })
 end
 
