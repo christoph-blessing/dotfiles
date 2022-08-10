cce9ab24aae4c8d84f4baa6eae6971f2a807d676
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-10 11:31:36 +0200

    Add keybinding for toggling quickfix window

--- dotfiles/nvim/.config/nvim/lua/keymappings.lua
+++ dotfiles/nvim/.config/nvim/lua/keymappings.lua
@@ -6,3 +6,5 @@ vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { silent = true })
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { silent = true })
+
+vim.api.nvim_set_keymap("n", "<C-q>", "<cmd>:call QuickFixToggle()<cr>", { silent = true })
