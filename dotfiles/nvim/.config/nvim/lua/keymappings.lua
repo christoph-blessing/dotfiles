a7b169c1d7e416d27880e1384cc68e5cc125a87f
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-10 12:34:37 +0200

    Add options to keymappings

--- dotfiles/nvim/.config/nvim/lua/keymappings.lua
+++ dotfiles/nvim/.config/nvim/lua/keymappings.lua
@@ -1,10 +1,12 @@
 vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true })
+local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { silent = true })
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", opts)
 
-vim.api.nvim_set_keymap("n", "<C-q>", "<cmd>:call QuickFixToggle()<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
+
+vim.api.nvim_set_keymap("n", "<C-q>", "<cmd>:call QuickFixToggle()<cr>", opts)
