9c449cce375445000118ad63662197125af999fa
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-12 14:37:24 +0200

    Update fzf config
    - Move configuration logic from zshenv to zshrc
    - List hidden files/directories

--- dotfiles/zsh/.config/zsh/zsh.d/fzf.zsh
+++ dotfiles/zsh/.config/zsh/zsh.d/fzf.zsh
@@ -1,4 +1,9 @@
 if [ -x "$(whence -p fzf)" ]; then
     source /usr/share/fzf/key-bindings.zsh
     source /usr/share/fzf/completion.zsh
+
+    export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+    export FZF_CTRL_T_COMMAND="fd --strip-cwd-prefix --hidden --follow --exclude .git"
+    export FZF_ALT_C_COMMAND="fd --type d --strip-cwd-prefix --hidden --follow --exclude .git"
+    export FZF_TMUX_OPTS="-d 40%"
 fi
