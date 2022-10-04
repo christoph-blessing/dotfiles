fbd06a592df40f8df531841b044a72bb6b5fa0c2
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-10-04 11:13:50 +0200

    Remove FixCursorHold

--- dotfiles/nvim/.config/nvim/lua/plugins.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins.lua
@@ -123,13 +123,11 @@ local plugins = {
 			require("b-notify").setup()
 		end,
 	},
-	{ "antoinemadec/FixCursorHold.nvim" },
 	{
 		"nvim-neotest/neotest",
 		requires = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
-			"antoinemadec/FixCursorHold.nvim",
 		},
 		config = function()
 			require("b-neotest").setup()
