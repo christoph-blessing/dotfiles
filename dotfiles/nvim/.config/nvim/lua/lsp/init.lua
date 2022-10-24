4dcaa76e00e6830775e42c628964222ce3bf443f
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-10-24 16:37:29 +0200

    Fix deprecated function call

--- dotfiles/nvim/.config/nvim/lua/lsp/init.lua
+++ dotfiles/nvim/.config/nvim/lua/lsp/init.lua
@@ -33,7 +33,7 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
 	properties = { "documentation", "detail", "additionalTextEdits" },
 }
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local lsp_installer = require("nvim-lsp-installer")
 
