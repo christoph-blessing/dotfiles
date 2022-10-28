e3bdb489e9e927293c1942dcdb80c06f478711ea
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-10-28 17:18:41 +0200

    Update illuminate config

--- dotfiles/nvim/.config/nvim/lua/b-illuminate.lua
+++ dotfiles/nvim/.config/nvim/lua/b-illuminate.lua
@@ -1,9 +1,20 @@
 M = {}
 
 M.setup = function()
-	vim.api.nvim_set_hl(0, "IlluminatedWordText", { standout = true })
-	vim.api.nvim_set_hl(0, "IlluminatedWordRead", { standout = true })
-	vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { standout = true, bold = true })
+	vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#7c6f64" })
+	vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#7c6f64" })
+	vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#7c6f64", bold = true })
+	require("illuminate").configure({
+		filetypes_denylist = {
+			"TelescopePrompt",
+			"dapui_watches",
+			"dapui_scopes",
+			"dapui_breakpoints",
+			"dapui_stacks",
+			"dap-repl",
+			"dapui_console",
+		},
+	})
 end
 
 return M
