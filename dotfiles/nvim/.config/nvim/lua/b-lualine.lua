f00598a444aa75b0aa09371899c0b7a96fa92dfe
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-11-04 11:37:52 +0100

    Remove filename from status line
    Having the filename in the status line and the window bar is redundant.

--- dotfiles/nvim/.config/nvim/lua/b-lualine.lua
+++ dotfiles/nvim/.config/nvim/lua/b-lualine.lua
@@ -12,7 +12,7 @@ M.setup = function()
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
-			lualine_c = { "filename" },
+			lualine_c = {},
 			lualine_x = { "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
@@ -20,7 +20,7 @@ M.setup = function()
 		inactive_sections = {
 			lualine_a = {},
 			lualine_b = {},
-			lualine_c = { "filename" },
+			lualine_c = {},
 			lualine_x = { "location" },
 			lualine_y = {},
 			lualine_z = {},
