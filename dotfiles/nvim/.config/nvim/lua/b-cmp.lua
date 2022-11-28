eae8dcf18ea868402877a15e941e2a9acb818461
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-11-28 17:34:56 +0100

    Fix broken snippet jumping in select mode

--- dotfiles/nvim/.config/nvim/lua/b-cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/b-cmp.lua
@@ -26,7 +26,10 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end),
+		end, {
+			"i",
+			"s",
+		}),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
@@ -35,7 +38,10 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end),
+		end, {
+			"i",
+			"s",
+		}),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
