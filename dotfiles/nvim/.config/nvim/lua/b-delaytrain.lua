05163675d54f9b005f80f619ed775b61cbcb639b
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-01-23 11:25:01 +0100

    [nvim] Ignore more filetypes in delaytrain config

--- dotfiles/nvim/.config/nvim/lua/b-delaytrain.lua
+++ dotfiles/nvim/.config/nvim/lua/b-delaytrain.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
 	require("delaytrain").setup({
-		ignore_filetypes = { "NvimTree", "toggleterm", "help" },
+		ignore_filetypes = { "NvimTree", "toggleterm", "help", "neotest-output", "neotest-summary" },
 	})
 end
 
