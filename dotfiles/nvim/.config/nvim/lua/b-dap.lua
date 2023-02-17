2ae3ff3605d3733b5bc35c2ac20c6c251e824fbc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-02-17 17:35:48 +0100

    [nvim] Update nvim-dap configuration
    - Point nvim-dap to python in which mason installs debugpy
    - Run debugees in virtual environment if active

--- dotfiles/nvim/.config/nvim/lua/b-dap.lua
+++ dotfiles/nvim/.config/nvim/lua/b-dap.lua
@@ -1,10 +1,10 @@
-M = {}
+local M = {}
 
 M.setup = function()
 	local dap = require("dap")
 	dap.adapters.python = {
 		type = "executable",
-		command = "python",
+		command = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python",
 		args = { "-m", "debugpy.adapter" },
 	}
 
@@ -15,6 +15,9 @@ M.setup = function()
 			name = "Launch file",
 			program = "${file}",
 			pythonPath = function()
+				if vim.fn.executable(os.getenv("VIRTUAL_ENV") .. "/bin/python") == 1 then
+					return os.getenv("VIRTUAL_ENV") .. "/bin/python"
+				end
 				return "/usr/bin/python"
 			end,
 		},
