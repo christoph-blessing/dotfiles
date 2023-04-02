f9648f1842d97d535d769055a4a7d3339ca9e2c5
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-04-02 22:03:37 +0200

    [nvim] Remove window switching keybinds

--- dotfiles/nvim/.config/nvim/lua/keymappings.lua
+++ dotfiles/nvim/.config/nvim/lua/keymappings.lua
@@ -4,9 +4,4 @@ local opts = { noremap = true, silent = true }
 
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", opts)
 
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
-
 vim.api.nvim_set_keymap("n", "<C-q>", "<cmd>:call QuickFixToggle()<cr>", opts)
