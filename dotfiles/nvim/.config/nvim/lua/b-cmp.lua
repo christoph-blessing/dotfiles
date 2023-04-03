7fcd7714b0a73cf0b05e3a5896177948cb8f3a95
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-04-03 20:56:13 +0200

    [nvim] Update confirm keybind for autocomplete

--- dotfiles/nvim/.config/nvim/lua/b-cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/b-cmp.lua
@@ -16,7 +16,7 @@ cmp.setup({
 	},
 
 	mapping = {
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
