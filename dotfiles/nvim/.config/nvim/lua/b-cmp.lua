612130f9a567adaf2dc7e0fd1adce30f805d1198
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-04-03 10:48:04 +0200

    [nvim] Change autocomplete keybindings

--- dotfiles/nvim/.config/nvim/lua/b-cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/b-cmp.lua
@@ -16,37 +16,13 @@ cmp.setup({
 	},
 
 	mapping = {
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
 	},
 
 	sources = {
