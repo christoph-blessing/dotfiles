2f79695b093b82aa0ebb70a74d15d9038e301cbf
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-04-14 16:03:17 +0200

    [nvim] Update settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,24 +1,28 @@
 vim.opt["clipboard"] = "unnamedplus"
-vim.opt["updatetime"] = 100
+vim.opt["updatetime"] = 50
 vim.opt["timeoutlen"] = 200
 vim.opt["termguicolors"] = true
 vim.opt["hidden"] = true
+
 vim.opt["splitbelow"] = true
 vim.opt["splitright"] = true
+
 vim.opt["showmode"] = false
 vim.opt["shortmess"] = vim.o.shortmess .. "c"
-vim.opt["shell"] = "zsh"
 vim.opt["mouse"] = ""
-
 vim.opt["spell"] = true
+
 vim.opt["number"] = true
 vim.opt["relativenumber"] = true
+
 vim.opt["wrap"] = false
 vim.opt["signcolumn"] = "yes"
+vim.opt["spelllang"] = "en_us"
+vim.opt["scrolloff"] = 8
 
 vim.opt["expandtab"] = true
-vim.opt["spelllang"] = "en_us"
 vim.opt["shiftwidth"] = 4
 vim.opt["tabstop"] = 4
+vim.opt["softtabstop"] = 4
 
 vim.g.python3_host_prog = require("provider").python()
