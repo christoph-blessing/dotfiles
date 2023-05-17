b97c8574cd68cdf753d4c64dee2c25bdf1acdbfc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-05-17 13:10:38 +0200

    [nvim]  Improve showing of plantuml diagrams
    - Create temporary .png to show from buffer content
    - Execute shell commands silently

--- nvim/.config/nvim/lua/plantuml.lua
+++ nvim/.config/nvim/lua/plantuml.lua
@@ -1,11 +1,24 @@
 local M = {}
 
+local get_buffer = function()
+	local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+	return table.concat(content, "\n")
+end
+
 M.show_diagram = function()
-	vim.cmd("!plantuml %")
-	local plantuml_filename = vim.fn.expand("%:t")
-	local extension = vim.fn.expand("%:e")
-	local diagram_filename, _ = string.gsub(plantuml_filename, extension, "png")
-	vim.cmd("!sxiv " .. diagram_filename)
+	local path = os.tmpname()
+	local file, error = io.open(path, "w")
+	if file then
+		file:write(get_buffer())
+		file:close()
+	else
+		print("Error opening file: " .. error)
+	end
+	vim.api.nvim_cmd({ cmd = "!", args = { "plantuml", path }, mods = { silent = true } }, {})
+	os.remove(path)
+	local image_path = path .. ".png"
+	vim.api.nvim_cmd({ cmd = "! ", args = { "sxiv", image_path }, mods = { silent = true } }, {})
+	os.remove(path .. image_path)
 end
 
 return M
