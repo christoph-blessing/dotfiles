46b0770b8b8c6025710c5dc057b32e1f15366503
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-05-26 17:30:11 +0200

    [nvim] Set markdown preview keybind on BufEnter

--- nvim/.config/nvim/lua/b-markdown-preview.lua
+++ nvim/.config/nvim/lua/b-markdown-preview.lua
@@ -2,10 +2,23 @@ local M = {}
 
 M.setup = function()
 	vim.cmd("let g:mkdp_browser = 'firefox'")
-	require("which-key").register(
-		{ r = { "<cmd>MarkdownPreviewToggle<cr>", "Toggle Markdown Preview" } },
-		{ prefix = "<leader>" }
-	)
+	local autocmd = require("utils.autocmd")
+	local wk = require("which-key")
+	autocmd.define_autocmds({
+		{
+			"BufEnter",
+			{
+				group = "markdown",
+				pattern = "*.md",
+				callback = function()
+					wk.register(
+						{ r = { "<cmd>MarkdownPreviewToggle<cr>", "Toggle Markdown Preview" } },
+						{ prefix = "<leader>" }
+					)
+				end,
+			},
+		},
+	})
 end
 
 return M
