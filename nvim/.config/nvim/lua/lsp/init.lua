2b2254d09188246e2b8fb65bc0d133abdcd196dc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-06-21 13:32:18 +0200

    [nvim] Remove unnecessary lsp config

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,19 +1,4 @@
-vim.diagnostic.config({ float = { source = "always" }, virtual_text = { prefix = "💡" } })
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = true,
-	signs = true,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = true,
-})
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = { "documentation", "detail", "additionalTextEdits" },
-}
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local on_attach = function(client, bufnr)
 	if client.server_capabilities.documentSymbolProvider then
