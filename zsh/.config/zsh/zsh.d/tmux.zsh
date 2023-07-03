c7c4ed3aceb439aa74bb15ce58a0b7e8447a8107
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-07-03 12:48:06 +0200

    [zsh] Remove tmux configuration

--- zsh/.config/zsh/zsh.d/tmux.zsh
+++
@@ -1,6 +0,0 @@
-# If tmux is executable, X is running, and not inside a tmux session, then try to attach.
-# If attachment fails, start a new session
-if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ]; then
-  [ -z "${TMUX}" ] && (tmux attach || tmux) >/dev/null 2>&1
-fi
-
