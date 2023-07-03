0274bb60f1a376610a8e22ce7c49acda5563ceba
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-07-03 12:45:57 +0200

    [nvim] Remove jj escape keybind

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -2,6 +2,4 @@ vim.g.mapleader = " "
 
 local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", opts)
-
 vim.api.nvim_set_keymap("n", "<C-q>", "<cmd>:call QuickFixToggle()<cr>", opts)
