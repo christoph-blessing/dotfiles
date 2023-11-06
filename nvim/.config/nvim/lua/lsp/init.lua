224ad68e1432dfd35d899eccaab31279632c44cc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-11-06 14:32:00 +0100

    [nvim] Remove nvim-navic residual

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,23 +1,16 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local on_attach = function(client, bufnr)
-	if client.server_capabilities.documentSymbolProvider then
-		require("nvim-navic").attach(client, bufnr)
-	end
-end
 
 require("lspconfig")["lua_ls"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = { Lua = { diagnostics = { globals = { "vim", "use" } } } },
+    capabilities = capabilities,
+    settings = { Lua = { diagnostics = { globals = { "vim", "use" } } } },
 })
 require("lspconfig")["pyright"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	handlers = { ["textDocument/publishDiagnostics"] = function() end },
+    capabilities = capabilities,
+    handlers = { ["textDocument/publishDiagnostics"] = function() end },
 })
 require("lspconfig")["bashls"].setup({})
 require("lspconfig")["rust_analyzer"].setup({})
-require("lspconfig")["fsautocomplete"].setup({ on_attach = on_attach, capabilities = capabilities })
+require("lspconfig")["fsautocomplete"].setup({ capabilities = capabilities })
 
 require("lsp.b-null-ls").setup()
